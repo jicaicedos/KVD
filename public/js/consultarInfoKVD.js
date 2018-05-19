a96e5afe28aecdb7af27e27e54004c1e92969122
Javier Caicedo <jicaicedocpe@gmail.com>
2018-05-19 17:49:13 -0500

    Actualizada Base de Gestores y KVD - 18 Mayo 2018

--- public/js/consultarInfoKVD.js
+++ public/js/consultarInfoKVD.js
@@ -12,45 +12,49 @@ function consultarInfoKVD() {
 	var idbeneficiario = $('#buscar').val()
 
     // Se cargan los datos a partir de un archivo JSON
-    $.getJSON("bd/K2GestoresMarzo-UTF8.json", function(datos) 
+    $.getJSON("../bd/base-kvd-mayo-18.json", function(datos) 
 	{
         // Se recorren uno a uno los registros en el archivo JSON
         $.each(datos["K2 Gestores Marzo"], function(idx,KVD) {
         	
             // Si un registro coincide con el criterio de búsqueda se obtienen los datos de ese registro
-        	if ( KVD["id beneficiario"] == idbeneficiario ) {
+        	if ( KVD["ID BENEFICIARIO"] == idbeneficiario ) {
 
                 // Copiamos los datos en el campo "gestores" del html: VerInformacionKVD.html
-        		$("#gestores").html("<h4>Códigos de identificación del KVD</h4>" +
+        		$("#gestores").html("<h4>Kiosco Vive Digital</h4>" +
                                 "<div class='separador'></div>" + 
-        						"<p>ID Beneficiario:    " + KVD["id beneficiario"] + "</p>" +
-        						"<p>ID Punto:    "+ KVD["id punto"] +
+                                "<h4>Contratista</h4>" +
+                                "<p>Contratista:    " + KVD["CONTRATISTA"] + "</p>" +
+
+                                "<h4>Códigos KVD</h4>" +
+                                "<div class='separador'></div>" + 
+        						"<p>ID Beneficiario:    " + KVD["ID BENEFICIARIO"] + "</p>" +
+        						"<p>ID Punto:    "+ KVD["ID Punto"] +
 
         						"<h4>Ubicación del Kiosco Vive Digital</h4>" +
                                 "<div class='separador'></div>" + 
-        						"<p>Departamento:     "+  KVD["departamento"] + "</p>" +
-        						"<p>Municipio:     "+  unescape(KVD["municipio"]) + "</p>" +
-        						"<p>Centro poblado:     " + KVD["centro poblado"] + "</p>" +
-        						"<p>Institución Educativa:     " + KVD["nombre institucion"] + "</p>" +
-        						"<p> Sede:     " + KVD["nombre sede"] + "</p>" +
-        						"<p>Dirección:     "+ KVD["direccion de la sede educativa"] + "</p>" +
+        						"<p>Departamento:     "+  KVD["Departamento"] + "</p>" +
+        						"<p>Municipio:     "+  unescape(KVD["Municipio"]) + "</p>" +
+        						"<p>Centro poblado:     " + KVD["Centro poblado"] + "</p>" +
+        						"<p>Institución Educativa:     " + KVD["Nombre Institución"] + "</p>" +
+        						"<p> Sede:     " + KVD["Nombre Sede"] + "</p>" +
+        						"<p>Dirección:     "+ KVD["Dirección de la sede educativa"] + "</p>" +
 
         						"<h4>Líneas telefónicas</h4>" +
                                 "<div class='separador'></div>" + 
-        						"<p>Línea cabina:     "+ KVD["linea cabina "] + "</p>" +
-        						"<p>Línea interna:     "+ KVD["linea interna"] + "</p>" +
+        						"<p>Teléfono KVD:     "+ KVD["TELÉFONO KVD"] + "</p>" +
 
         						"<h4>Datos Gestor(a)</h4>" +
                                 "<div class='separador'></div>" + 
-        						"<p>Nombre:     " + KVD["nombres gestor"] + "</p>" +
-        						"<p>Cédula:     " + KVD["cedula gestor"] + "</p>" +
-        						"<p>Celular:     " + KVD["Celular gestor"] + "</p>"  +
-                                "<p>Correo electrónico:     " + KVD["correo electrónico  gestor"] + "</p>" + 
+        						"<p>Nombre:     " + KVD["GESTOR"] + "</p>" +
+        						// "<p>Cédula:     " + KVD["cedula gestor"] + "</p>" +
+        						"<p>Celular:     " + KVD["TELÉFONO MOVIL GESTOR"] + "</p>"  +
+                                "<p>Correo electrónico:     " + KVD["CORREO ELECTRÓNICO  GESTOR"] + "</p>" + 
 
                                 "<h4>Coordinador</h4>" +
                                 "<div class='separador'></div>" + 
-                                "<p>Nombre:     "+ KVD["coordinador"] + "</p>" +
-                                "<p>Celular coordinador:     "+ KVD["telefono contacto"] + "</p>"
+                                "<p>Nombre:     "+ KVD["COORDINADOR"] + "</p>" +
+                                "<p>Celular coordinador:     "+ KVD["TELEFONO CONTACTO"] + "</p>"
 
                             )
                 return false 
